4e941f0f02d2bbc700410da8651852dcba05fb99
kkcarry123 <China-Officical-Government@.github.com>
2024-06-23 23:55:23 +0800

    Import glm library

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void process_input(GLFWwindow* window);
@@ -8,6 +11,19 @@ void process_input(GLFWwindow* window);
 int main()
 {
 	//std::cout << "Hellow world" << std::endl;
+
+	// glm test
+	glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+	glm::mat4 trans = glm::mat4(1.0f); // identity matrix
+	//trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+	//trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0f)); // rotation
+	trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f)); // scaling
+	vec = trans * vec;
+
+	std::cout << vec.x << " " << vec.y << " " << vec.z << std::endl;
+
+
+
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
